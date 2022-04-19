6f90f596034ac01f3631651d9ba1665a3dbf028a
loremczuk <l.oremczuk@gmail.com>
2022-04-19 20:29:37 +0200

    Initial commit

---
+++ src/main/java/pl/oremczuk/springsecurityldap/SimpleApi.java
@@ -0,0 +1,15 @@
+package pl.oremczuk.springsecurityldap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleApi {
+
+    @GetMapping("/")
+    public String homeWelcome() {
+        return "<h1>Welcome</h1>";
+    }
+
+
+}
---
+++ src/test/java/pl/oremczuk/springsecurityldap/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.oremczuk.springsecurityldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
